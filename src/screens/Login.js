4cb37dfbb9ad9eaf0332942dbc58bf7600eef9b5
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-19 11:26:44 -0300

    Login creo que termine

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -3,17 +3,71 @@ import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import Layout from '../Componentes/Layout';
 import useEffect from 'react'
 const Login = ({ navigation }) => {
-    // No esta hecho, no entendi donde guardar el usuario
+
+    const { usuario, setUsuario } = ({})
+    useEffect(() => setUsuario(null), [])
+
+    const [login, setlogin] = useState({})
+    const [error, setError] = useState('')
+
+    const handleEmailChange = (email) => setlogin({ ...login, email })
+    const handlePasswordChange = (password) => setlogin({ ...login, password })
+
+    const handleLogin = () => {
+        const postData = async () => {
+            try {
+                const response = await fetch('http://challenge-react.alkemy.org/', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(login)
+                })
+                if (!response.ok) throw new Error('Error en la peticion')
+                setUsuario(login)
+                navigation.navigate('Home')
+            } catch (error) {
+                setError('Error en la peticion')
+                console.error(error)
+            }
+        }
+        postData()
+    }
+
     return (
-        <Layout >
+        <Layout>
             <View style={styles.container}>
-                <Button  title="Ir a la Home" onPress={() => navigation.navigate('Home')} />
+                <Text style={styles.label}>Correo Electrónico:</Text>
+                <TextInput
+                    placeholder="blabla@lolsas.com"
+                    onChangeText={handleEmailChange}
+                    value={login.email || ''}
+                />
+                <Text style={styles.label}>Contraseña:</Text>
+                <TextInput
+
+                    placeholder="12345"
+                    value={login.password || ''}
+                    onChangeText={handlePasswordChange}
+                    secureTextEntry 
+                />
+                <Button title="Iniciar Sesión" onPress={handleLogin} />
+
+                <View><Text>{error}</Text></View>
+
+            </View>
+            {/*Por si no anda el fetch*/}
+            <View>
+                <Button title="Continuar Sin Logearse" onPress={() => navigation.navigate('Home')} />
             </View>
         </Layout>
     )
 }
 
 const styles = StyleSheet.create({
+    label: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginBottom: 5,
+    },
     container: {
         padding: 20,
         width: '100%',
